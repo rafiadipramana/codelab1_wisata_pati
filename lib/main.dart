d4103467cd9a763bdff55fdb6f12e79156cd1fc4
rafiadipramana <rafiadipramana@gmail.com>
2023-01-10 10:16:14 +0700

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisata Pati',
+      title: 'Tempat Wisata Pati',
       theme: ThemeData(),
       home: MainScreen(),
       debugShowCheckedModeBanner: false,
